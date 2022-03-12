891c3729ea5925a7de9a19015f2f67355fc008b1
hugosenari <hugosenari@gmail.com>
2022-03-12 04:04:26 -0300

    chore: re index sha1

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''SHA-1 produces a 160-bit (20-byte) hash value from arbitrary input'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-sha1-master.flake = false;
+  inputs.src-sha1-master.owner = "onionhammer";
+  inputs.src-sha1-master.ref   = "refs/heads/master";
+  inputs.src-sha1-master.repo  = "sha1";
+  inputs.src-sha1-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-sha1-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-sha1-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
